e9525d5cc2760f7ed0f29954dfe3961d89d8ac2d
hy02115 <huyang02115@yahoo.com>
2017-03-31 21:44:29 -0400

    Get Functions of Constant Failure Rate Model

--- +const_flr_rate/getFunc.m
+++ +const_flr_rate/getFunc.m
@@ -1,4 +1,7 @@
 function [flr_rate, relia_func, flr_density] = getFunc(t, para)
+    validateattributes(t, {'numeric'}, {'>=', 0});
+    validateattributes(para, {'numeric'}, {'>', 0});
+    
     relia_func = exp(-para * t);
     flr_rate = para;
     flr_density = para * exp(-para * t); 
